b279f97507b3259822485a0d45643df38f612272
DavidNodeB <db1416db@gmail.com>
2023-09-04 22:44:55 -0400

    first commit

---
+++ src/script.js
@@ -0,0 +1,28 @@
+const rock = document.querySelector(".rock"); 
+const paper = document.querySelector(".paper"); 
+const scissors = document.querySelector(".scissors"); 
+let score = document.querySelector(".score");
+const result = document.querySelector(".result");
+const choices = ["rock", "paper", "scissors"]; 
+let movesleft = 3;
+const player = [rock, paper, scissors]; 
+let computer = choices[Math.floor(Math.random() * choices.length)]; 
+
+
+function Game() {
+    if (player == computer) {
+        result.innerText = 'Tie'; 
+    } else if (player == "paper") {
+        if (computer == "scissors") {
+            result.innerText = "Computer wins";
+        }
+    } else if (player == "rock") {
+        if (computer == "paper") {
+            result.innerText = "Computer wins"; 
+        }
+    } else if (player == "scissors") {
+        if (computer == "rock") {
+            result.innerText = "Computer wins"; 
+        }
+    }
+}
